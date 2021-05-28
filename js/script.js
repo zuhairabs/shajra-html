d2b61c564cce96288eeb93ea4898dfae60d67396
Zuhair <zuhairabs@gmail.com>
2021-05-28 19:31:59 +0530

    Initialized repo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var elem = document.getElementById("graph");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
