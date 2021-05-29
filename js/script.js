c3aad8d387ad3d19d2e974c15f63bebf07ac6146
Zuhair Abbas <zuhairabs@gmail.com>
2021-05-29 20:26:41 +0530

    Updated Shajra Json | Dark Mode | Path Issue Created

--- js/script.js
+++ js/script.js
@@ -8,3 +8,35 @@ function openFullscreen() {
     elem.msRequestFullscreen();
   }
 }
+
+ /* -----------------------------------
+           dark-mode
+    ----------------------------------- */
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    const logoDark = document.querySelector('.logo-dark');
+    const logoWhite = document.querySelector('.logo-white');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+
+        if (currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+            document.body.classList.toggle("dark");
+        }
+    }
+
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+            document.body.classList.add('dark');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+            document.body.classList.remove('dark');
+        }
+    }
+
+    toggleSwitch.addEventListener('change', switchTheme, false);
